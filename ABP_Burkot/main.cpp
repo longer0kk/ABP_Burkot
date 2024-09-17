50b2c503e59f09cfa1fa1f66612c6a59d2fb21c5
longer0kk <vladikburkot@ukr.net>
2024-09-17 20:52:47 +0300

    Репозиторій

---
+++ ABP_Burkot/main.cpp
@@ -0,0 +1,20 @@
+/* #include "lab_1/lab_1.cpp"
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+	int lab_num;
+anchor:
+	cout << "Give me number of the lab you want to run" << endl;
+	cin >> lab_num;
+
+	switch (lab_num) {
+	case 1:
+		lab1();
+		break;
+	case 0:
+		return 0;
+	}
+	goto anchor;
+} */
